d87f94b5d9c9c112373c37aac5b71e5405e89089
bluolightning <124224350+bluolightning@users.noreply.github.com>
2024-12-15 13:34:23 -0500

    Initial commit

---
+++ content.js
@@ -0,0 +1,7 @@
+const divvvvvvy = document.createElement('div');
+
+divvvvvvy.setAttribute('style', 'background-color: black; width: 100vw; height: 100vh; position: absolute; top: 0px; left: 0px;')
+
+document.querySelector('body').appendChild(divvvvvvy);
+
+console.log('done');
